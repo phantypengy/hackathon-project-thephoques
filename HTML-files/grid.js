a11f7a27c4ca13eb8eff9efccdeb1bc19185b79d
Dominik Chwirot <dchwirot01@gmail.com>
2026-01-30 16:24:10 +0000

    Add files via upload

---
+++ HTML-files/grid.js
@@ -0,0 +1,5 @@
+function playsong1() {
+  document.getElementById("nowPlaying.Title").innerText = "Mayonaise";
+  document.getElementById("nowPlaying.Artist").innerText =
+    "The Smashing Pumpkins";
+}
