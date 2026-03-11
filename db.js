7b2b674fedcca0b2932b7d80aad33615047b3f1a
Dom <dchwirot01@gmail.com>
2026-03-11 10:14:32 +0000

    added macOS / linux check to db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+const os = require("os");
 
 const pool = new Pool({
-    database: 'sealio_database',
-    user: 'sealio',
-    password: '',
-    host: '/var/run/postgresql',
+  database: "sealio_database",
+  user: os.userInfo().username,
+  host: os.platform() === "darwin" ? "/tmp" : "/var/run/postgresql",
 });
 
 module.exports = pool;
